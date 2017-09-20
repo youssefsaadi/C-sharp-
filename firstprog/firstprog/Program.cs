27be709e1242f0a28f2e9a96c2b764f24be1c51f
youssefsaadi <32045021+youssefsaadi@users.noreply.github.com>
2017-09-21 01:18:21 +0200

    first C sharp program
    this is my first C sharp program

---
+++ firstprog/firstprog/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace firstprog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // THIS IS MY FIRST PROGRAM IN C#
+            Console.WriteLine("hello word");
+            Console.ReadKey();
+        }
+    }
+}
