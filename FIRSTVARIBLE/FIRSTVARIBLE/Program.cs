b7bca4912993d25ec75db17e320ae0ae62cd8775
youssefsaadi <32045021+youssefsaadi@users.noreply.github.com>
2017-09-21 01:28:41 +0200

    my first variable
    this is my first variable

---
+++ FIRSTVARIBLE/FIRSTVARIBLE/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIRSTVARIBLE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 25;
+            Console.WriteLine("my variable value = {0}", x);
+            Console.ReadKey();
+        }
+    }
+}
